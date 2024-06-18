47bffee7bc1764f61731023c487ac7275bd1ba0f
Maycon Douglas <85197511+WzK000@users.noreply.github.com>
2024-06-17 22:53:04 -0300

    Primeiras aulas
    Na aula 5 fizemos o primeiro HelloWorld na linguagem Java;
    Na aula 6 aprendemos a passar um argumento;

---
+++ Aula 5/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main (String[] args){
+    System.out.println("Hello World!!");
+    }
+}
+
+/* Classe: Class
+HelloWorld: Nome da Classe
+Public static...: Método
+ */
---
+++ Aula 5/PrimeiroExercicio.java
@@ -0,0 +1,5 @@
+class PrimeiroExercicio{
+    public static void main (String[] args){
+    System.out.println("Olá, mundo!");
+    }
+}
